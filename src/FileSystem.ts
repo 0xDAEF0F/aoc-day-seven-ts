bbcc40575715405870eb2b2e80687969a14edc19
daemon <0xDAEF0F@proton.me>
2023-10-09 15:10:44 -0600

    checkpoint

--- src/FileSystem.ts
+++ src/FileSystem.ts
@@ -1,3 +1,31 @@
-export class FileSystem {
-    constructor() {}
+import { set } from 'lodash'
+
+interface Asset {
+    name: string;
+    type: 'file' | 'directory';
+    size?: number;
+    children?: { [name: string]: Asset };
+}
+
+export class MyFileSystem {
+    currentPath: string[] = []
+    children: Asset = { name: '/', type: 'directory' }
+
+    constructor() {
+        this.currentPath.push("/")
+    }
+
+    addDirectory(name: string) {
+        // how deeply nested are we?
+        const newDir: Asset = { name, type: 'directory', children: {} }
+        const levelsDeep = this.currentPath.length
+
+        const a = "children.".repeat(levelsDeep).slice(0, -1)
+        set(this.children, a, newDir)
+    }
+
+    addFile(name: string, size: number) {
+        const newFile: Asset = { name, size, type: 'file' }
+
+    }
 }
--- src/index.ts
+++ src/index.ts
@@ -3,11 +3,67 @@ import path from 'path'
 import { promises as fs } from 'fs'
 dotenv.config()
 
+type FileSystemNode = {
+    name: string;
+    type: 'file' | 'directory';
+    size?: number;
+    children?: { [name: string]: FileSystemNode };
+}
+
 async function main() {
     const filePath = path.join(__dirname, '..', 'input-day-7.txt')
     const data = await fs.readFile(filePath, 'utf8')
+    const lines = data.split('\n')
 
-    console.log(data)
+    const instructionChunks: string[][] = [[]]
+
+    lines.forEach((l) => {
+        if (!l.startsWith("$")) {
+            instructionChunks[instructionChunks.length - 1].push(l)
+        } else {
+            instructionChunks.push([l])
+        }
+    })
+
+    let fileSystem: FileSystemNode = {
+        name: "/",
+        type: 'directory',
+        children: {}
+    }
+
+    let pathState = ["/"]
+
+    instructionChunks.forEach((chunk) => {
+        if (chunk[0].startsWith("$ cd")) {
+            // change `pathState`
+            const nextDest = chunk[0].slice(5)
+            const goParent = nextDest === '..'
+
+            if (goParent) pathState.pop()
+            else pathState.push(nextDest)
+
+        } else if (chunk[0].startsWith("$ ls")) {
+            // fill new children
+            chunk.forEach((node, i) => {
+                // skip first iteration (we know it is the instruction)
+                if (i === 0) return
+
+                if (node.startsWith("dir")) {
+                    // create directory inside the path
+                    const dirName = node.split(' ')[1]
+                } else {
+                    // it is a regular file (create it)
+                    const fileSize = +node.split(' ')[0]
+                    const fileName = node.split(' ')[1]
+                    const fileSystemNode: FileSystemNode = {
+                        type: "file",
+                        name: fileName,
+                        size: fileSize
+                    }
+                }
+            })
+        }
+    })
 }
 
 main()
