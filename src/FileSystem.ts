3cd35d276a16c5d5fa9faddbd03efb8f8cb5f063
daemon <0xDAEF0F@proton.me>
2023-10-14 10:06:24 -0600

    solved challenge a

--- src/FileSystem.ts
+++ src/FileSystem.ts
@@ -9,23 +9,64 @@ interface Asset {
 
 export class MyFileSystem {
     currentPath: string[] = []
-    children: Asset = { name: '/', type: 'directory' }
+    children: Record<string, Asset> = {}
+    totalSum: number = 0
 
-    constructor() {
-        this.currentPath.push("/")
-    }
+    constructor() { }
 
     addDirectory(name: string) {
-        // how deeply nested are we?
         const newDir: Asset = { name, type: 'directory', children: {} }
-        const levelsDeep = this.currentPath.length
 
-        const a = "children.".repeat(levelsDeep).slice(0, -1)
-        set(this.children, a, newDir)
+        if (this.currentPath.length === 0) {
+            set(this.children, name, newDir)
+        } else {
+            const path = this.currentPath.flatMap((n) => [n, "children"]).concat(name)
+            set(this.children, path, newDir)
+        }
     }
 
     addFile(name: string, size: number) {
         const newFile: Asset = { name, size, type: 'file' }
 
+        if (this.currentPath.length === 0) {
+            set(this.children, name, newFile)
+        } else {
+            const path = this.currentPath.flatMap((n) => [n, "children"]).concat(name)
+            set(this.children, path, newFile)
+        }
+    }
+
+    changeDirectory(where: string) {
+        if (where === '..') {
+            this.currentPath.pop()
+        } else {
+            this.currentPath.push(where)
+        }
+    }
+
+    findTotalSizeFileSystem() {
+        this.totalSum = 0
+        this.goFindSize(this.children)
+        return this.totalSum
+    }
+
+    private goFindSize(asset: Record<string, Asset>): number {
+        if (Object.keys(asset).length === 0) return 0
+
+        let totalSize = 0
+
+        for (let k in asset) {
+            if (asset[k].type === 'file') {
+                totalSize += asset[k].size!
+            } else if (asset[k].type === 'directory') {
+                totalSize += this.goFindSize(asset[k].children!)
+            }
+        }
+
+        if (totalSize <= 100_000) {
+            this.totalSum += totalSize
+        }
+
+        return totalSize
     }
 }
--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,15 @@
 import * as dotenv from 'dotenv'
 import path from 'path'
 import { promises as fs } from 'fs'
+import { MyFileSystem } from './FileSystem'
 dotenv.config()
-
-type FileSystemNode = {
-    name: string;
-    type: 'file' | 'directory';
-    size?: number;
-    children?: { [name: string]: FileSystemNode };
-}
 
 async function main() {
     const filePath = path.join(__dirname, '..', 'input-day-7.txt')
     const data = await fs.readFile(filePath, 'utf8')
     const lines = data.split('\n')
 
-    const instructionChunks: string[][] = [[]]
+    const instructionChunks: string[][] = []
 
     lines.forEach((l) => {
         if (!l.startsWith("$")) {
@@ -25,13 +19,7 @@ async function main() {
         }
     })
 
-    let fileSystem: FileSystemNode = {
-        name: "/",
-        type: 'directory',
-        children: {}
-    }
-
-    let pathState = ["/"]
+    const fileSystem = new MyFileSystem()
 
     instructionChunks.forEach((chunk) => {
         if (chunk[0].startsWith("$ cd")) {
@@ -39,31 +27,27 @@ async function main() {
             const nextDest = chunk[0].slice(5)
             const goParent = nextDest === '..'
 
-            if (goParent) pathState.pop()
-            else pathState.push(nextDest)
+            if (goParent) fileSystem.changeDirectory('..')
+            else fileSystem.changeDirectory(nextDest)
 
         } else if (chunk[0].startsWith("$ ls")) {
-            // fill new children
             chunk.forEach((node, i) => {
                 // skip first iteration (we know it is the instruction)
                 if (i === 0) return
 
                 if (node.startsWith("dir")) {
-                    // create directory inside the path
                     const dirName = node.split(' ')[1]
+                    fileSystem.addDirectory(dirName)
                 } else {
-                    // it is a regular file (create it)
                     const fileSize = +node.split(' ')[0]
                     const fileName = node.split(' ')[1]
-                    const fileSystemNode: FileSystemNode = {
-                        type: "file",
-                        name: fileName,
-                        size: fileSize
-                    }
+                    if (fileName) fileSystem.addFile(fileName, fileSize)
                 }
             })
         }
     })
+
+    console.log(fileSystem.findTotalSizeFileSystem())
 }
 
 main()
